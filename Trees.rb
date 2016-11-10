0abfc7d0afe9051eecc149a87b7298a239b8acb8
Chris <chris@adadevelopersacademy.org>
2016-11-10 08:06:30 -0800

    Added Trees.rb

---
+++ Trees.rb
@@ -0,0 +1,32 @@
+# Trees.rb
+
+# Use the Treenode class and manually setup a 5-node tree with your favorite TV Characters.
+#  Then do inorder, preorder and postorder traversal
+
+class Tree_Node
+  attr_accessor :value, :left, :right
+
+  def initialize(value, left = nil, right = nil)
+    @value = value
+    @left = left
+    @right = right
+  end
+
+  def inorder()
+    # Code Goes here
+  end
+  def postorder()
+    # Code Goes here
+  end
+  def preorder()
+    # Code Goes here
+  end
+end
+
+# Create 5 tree_nodes and link them up
+# draw the resulting tree
+
+
+
+
+# Create a set of Binary Expression Trees
